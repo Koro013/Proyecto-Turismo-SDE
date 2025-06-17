d2f71792a91dca081569fc398308af639ac69450
Facundo coronel <138814500+Koro013@users.noreply.github.com>
2025-06-17 01:52:32 -0300

    Delete Oficina-turismo directory

--- Oficina-turismo/lang/en.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'titulo' => 'Tourism Office of Santiago del Estero',
-    'bienvenida' => 'Welcome to Santiago del Estero',
-    'btn_explorar' => 'Explore Destinations',
-    'mision' => 'Offering authentic experiences that reflect our identity.',
-];
